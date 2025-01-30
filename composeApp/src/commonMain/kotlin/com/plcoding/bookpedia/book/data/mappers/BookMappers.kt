19e85b351c9d8034ace4d528011fb8060bdee8f6
Ozan Kalan <ozz4654@gmail.com>
2025-01-30 11:45:15 -0600

    [Book Repository]
    - created data sources and impl for MVI clean arch
    - created mapper for DTO to DataModel

---
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
@@ -0,0 +1,24 @@
+package com.plcoding.bookpedia.book.data.mappers
+
+import com.plcoding.bookpedia.book.data.dto.SearchedBookDto
+import com.plcoding.bookpedia.book.domain.Book
+
+fun SearchedBookDto.toBook(): Book {
+    return Book(
+        id = id,
+        title = title,
+        imgUrl = if (coverKey != null) {
+            "https://covers.openlibrary.org/b/olid/${coverKey}-L.jpg"
+        } else {
+            "https://covers.openlibrary.org/b/olid/${coverAlternativeKey}-L.jpg"
+        },
+        description = null,
+        authors = authorNames ?: emptyList(),
+        languages = languages ?: emptyList(),
+        firstPublishedYear = firstPublishedYear.toString(),
+        averageRating = ratingsAverage,
+        ratingCount = ratingsCount,
+        numPages = numPagesMedian,
+        numEditions = numEditions ?: 0,
+    )
+}
