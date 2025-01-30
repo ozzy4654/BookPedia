2a2bbd41562ec2e15f6af116860b847cdd35423a
Ozan Kalan <ozz4654@gmail.com>
2025-01-29 22:22:01 -0600

    Mock data for books

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_list/BookListState.kt
@@ -5,9 +5,26 @@ import com.plcoding.bookpedia.core.presentation.UiText
 
 data class BookListState(
     val searchQuery: String = "Kotlin",
-    val searchResults: List<Book> = emptyList(),
+    val searchResults: List<Book> = books,
     val favoriteBooks: List<Book> = emptyList(),
     val isLoading: Boolean = false,
     val selectedTabIndex: Int = 0,
     val errorMessage: UiText? = null,
     )
+
+val books = (1 ..100).map {
+    Book(
+        id = it.toString(),
+        title = "Book $it",
+        imgUrl = "http.test.com",
+        authors = listOf("Ozan Kalan"),
+        description = "Description: $it",
+        languages = emptyList(),
+        firstPublishedYear = null,
+        numPages = 100,
+        averageRating = 4.6979,
+        numEditions = 3,
+        ratingCount = 5
+
+    )
+}
