28335e2533ab2d230c25262b0840d9551e426db3
Ozan Kalan <ozz4654@gmail.com>
2025-01-31 23:47:46 -0600

    [Book Detail Background]
    - book elevated card
    - fav icon with click action

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/presentation/book_detail/components/BlurredImageBackground.kt
@@ -1,16 +1,26 @@
 package com.plcoding.bookpedia.book.presentation.book_detail.components
 
+import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.displayCutoutPadding
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -21,16 +31,22 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.blur
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import cmp_bookpedia.composeapp.generated.resources.Res
 import cmp_bookpedia.composeapp.generated.resources.book_cover
+import cmp_bookpedia.composeapp.generated.resources.book_error_2
 import cmp_bookpedia.composeapp.generated.resources.go_back
+import cmp_bookpedia.composeapp.generated.resources.mark_as_fav
+import cmp_bookpedia.composeapp.generated.resources.remove_from_fav
 import coil3.compose.rememberAsyncImagePainter
 import com.plcoding.bookpedia.core.presentation.DarkBlue
 import com.plcoding.bookpedia.core.presentation.DesertWhite
+import com.plcoding.bookpedia.core.presentation.SandYellow
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 @Composable
@@ -62,7 +78,7 @@ fun BlurredImageBackground(
 
     )
 
-    Box {
+    Box(modifier = modifier) {
         Column(
             modifier = Modifier
                 .fillMaxSize(),
@@ -96,10 +112,11 @@ fun BlurredImageBackground(
             }
         }
 
+        // TODO: Update color base on image blurr background color, so its always visiable
         IconButton(
             onClick = onBackClicked,
             modifier = Modifier.align(Alignment.TopStart)
-                .padding( start = 16.dp)
+                .padding(start = 16.dp)
                 .systemBarsPadding()
         ) {
             Icon(
@@ -108,5 +125,91 @@ fun BlurredImageBackground(
                 tint = Color.Black
             )
         }
+
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Spacer(
+                modifier = Modifier.fillMaxHeight(fraction = 0.15f)
+            )
+
+            ElevatedCard(
+                modifier = Modifier
+                    .height(230.dp)
+                    .aspectRatio(2 / 3f),
+                shape = RoundedCornerShape(8.dp),
+                colors = CardDefaults.elevatedCardColors(
+                    containerColor = Color.Transparent
+                ),
+                elevation = CardDefaults.cardElevation(15.dp),
+            ) {
+                AnimatedContent(targetState = imageLoadResult) { result ->
+                    when (result) {
+                        null -> CircularProgressIndicator()
+                        else -> {
+                            Box() {
+                                Image(
+                                    painter = if (result.isSuccess) {
+                                        painter
+                                    } else {
+                                        painterResource(Res.drawable.book_error_2)
+                                    },
+                                    contentDescription = stringResource(Res.string.book_cover),
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                        .background(Color.Transparent),
+                                    contentScale = if (result.isSuccess) {
+                                        ContentScale.Crop
+                                    } else {
+                                        ContentScale.Fit
+                                    },
+                                )
+
+                                IconButton(
+                                    modifier = Modifier
+                                        .align(Alignment.BottomEnd)
+                                        .background(
+                                            brush = Brush.radialGradient(
+                                                colors = listOf(
+                                                    SandYellow,
+                                                    Color.Transparent,
+                                                ),
+                                                radius = 70f
+                                            )
+                                        ),
+                                    onClick = {
+                                        onFavoriteClicked()
+                                    }
+                                ) {
+                                    Icon(
+                                        imageVector =
+                                            if (isFavorite) {
+                                                Icons.Default.Favorite
+                                            } else {
+                                                Icons.Outlined.FavoriteBorder
+                                            },
+                                        contentDescription = stringResource(
+                                            if (isFavorite) {
+                                                Res.string.remove_from_fav
+                                            } else {
+                                                Res.string.mark_as_fav
+                                            }
+                                        ),
+                                        tint = Color.Red,
+                                        modifier = Modifier
+                                    )
+                                }
+                            }
+
+                        }
+                    }
+
+                }
+            }
+
+        }
+
+        content()
     }
 }
