cc4391008b05a5c62290841f8ef352ae57a0669a
Ozan Kalan <ozz4654@gmail.com>
2025-02-02 00:00:29 -0600

    [Room Database]
    - Create our factory
    - Entity and Dao
    - Mappers for Entity and data models

--- composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
+++ composeApp/src/commonMain/kotlin/com/plcoding/bookpedia/book/data/mappers/BookMappers.kt
@@ -1,5 +1,6 @@
 package com.plcoding.bookpedia.book.data.mappers
 
+import com.plcoding.bookpedia.book.data.database.BookEntity
 import com.plcoding.bookpedia.book.data.dto.SearchedBookDto
 import com.plcoding.bookpedia.book.domain.Book
 
@@ -20,5 +21,37 @@ fun SearchedBookDto.toBook(): Book {
         ratingCount = ratingsCount,
         numPages = numPagesMedian,
         numEditions = numEditions ?: 0,
+    )
+}
+
+fun Book.toBookEntity(): BookEntity {
+    return BookEntity(
+        id = id,
+        title = title,
+        description = description.orEmpty(),
+        imageUrl = imgUrl,
+        languages = languages,
+        authors = authors,
+        firstPublishYear = firstPublishedYear,
+        ratingsAverage = averageRating,
+        ratingsCount = ratingCount,
+        numPagesMedian = numPages,
+        numEditions = numEditions
+    )
+}
+
+fun BookEntity.toBook(): Book {
+    return Book(
+        id = id,
+        title = title,
+        description = description.orEmpty(),
+        imgUrl = imageUrl,
+        languages = languages,
+        authors = authors,
+        firstPublishedYear = firstPublishYear,
+        averageRating = ratingsAverage,
+        ratingCount = ratingsCount,
+        numPages = numPagesMedian,
+        numEditions = numEditions
     )
 }
---
+++ composeApp/src/iosMain/kotlin/com/plcoding/bookpedia/book/data/database/DatabaseFactory.ios.kt
@@ -0,0 +1,31 @@
+@file:OptIn(ExperimentalForeignApi::class)
+
+package com.plcoding.bookpedia.book.data.database
+
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSUserDomainMask
+
+actual class DatabaseFactory {
+    actual fun create(): RoomDatabase.Builder<FavoriteBookDataBase> {
+        val dbFile = documentDirectory() + "/${FavoriteBookDataBase.DB_NAME}"
+        return Room.databaseBuilder<FavoriteBookDataBase>(
+            name = dbFile
+        )
+    }
+
+    private fun documentDirectory(): String {
+        val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null
+        )
+
+        return requireNotNull(documentDirectory?.path())
+    }
+}
